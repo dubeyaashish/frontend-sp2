87b3f79eb6a690a16e5f8fb17d718a9d8c2040f2
dubeyaashish <72406443+dubeyaashish@users.noreply.github.com>
2024-01-29 18:51:27 +0700

    29/01/23

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom'; // Import Navigate
 import LayoutWithNavigation from './LayoutNavigation';
 import StatusPage from './Status';
 import GeneralPage from './General';
 import SettingsPage from './Settings';
 import LoginPage from './Login';
-import UserGeneralPage from './UserGeneral'; // Import the UserGeneralPage component
+import UserGeneralPage from './UserGeneral';
 
 export default function App() {
   const [drawerOpen, setDrawerOpen] = useState(true);
@@ -19,13 +19,15 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={
         <LayoutWithNavigation open={drawerOpen} handleDrawerToggle={toggleDrawer}>
-          <Route index element={<GeneralPage />} />
-          <Route path="status" element={<StatusPage />} />
-          <Route path="settings" element={<SettingsPage />} />
-          {/* Add the route for UserGeneralPage */}
-          <Route path="user-general" element={<UserGeneralPage />} />
+          <Routes> {/* Nested Routes inside LayoutWithNavigation */}
+            <Route index element={<GeneralPage />} />
+            <Route path="status" element={<StatusPage />} />
+            <Route path="settings" element={<SettingsPage />} />
+            <Route path="user-general" element={<UserGeneralPage />} />
+          </Routes>
         </LayoutWithNavigation>
       } />
+      <Route path="*" element={<Navigate to="/" replace />} /> {/* Default Route */}
     </Routes>
   );
 }
